e671cf9a95c0d67b1851142519321aaf1d94e408
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2024-02-18 22:36:55 -0300

    doc: auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,36 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LogInDto } from './dto/login.dto';
 
+@ApiTags('Auth')
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Create a new JWT.' })
+  @ApiBody({ type: LogInDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Created.',
+    schema: {
+      example: {
+        access_token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVsaTI0QGVtYWlsZWxpLmNvbSIsInN1YiI6IjczOTU3OGYxLTg0MzctNGU0ZC1iODk0LTFiMDNjMTczNjJlZiIsImlhdCI6MTcwODMwMjUzOCwiZXhwIjoxNzA4MzA2MTM4fQ.5KaqoU5HdWbUWkq1CGXfeslSndktPdvDakDk9aSBH7Q',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized.',
+    schema: {
+      example: {
+        message: 'Unauthorized',
+        statusCode: 401,
+      },
+    },
+  })
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LogInDto {
+  @ApiProperty()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   @MinLength(8)
