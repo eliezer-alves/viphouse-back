d5f7332dd0f1b5879c79069e372f4cb8fe8e64af
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2024-02-18 22:24:19 -0300

    bugfix: email not registered validation

--- src/users/validation-rules/email-not-registered.rule.ts
+++ src/users/validation-rules/email-not-registered.rule.ts
@@ -11,6 +11,7 @@ export class IsEmailNotRegistered implements ValidatorConstraintInterface {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   validate(email: any) {
+    if (!email || typeof email !== 'string') return false;
     return this.usersRepository.findUniqueByEmail(email).then((user) => {
       return user === null;
     });
