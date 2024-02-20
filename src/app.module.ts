b2f25f603840e38e9f5ee12c4d49b7dc8a7e8418
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2024-02-20 09:35:10 -0300

    feat: basic properties resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PropertiesModule } from './properties/properties.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, PropertiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
