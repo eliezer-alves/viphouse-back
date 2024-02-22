1fbf60ee118741a612d26dc562755adaa88920ce
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2024-02-22 15:08:07 -0300

    feat: creating porperty module entities

---
+++ src/modules/properties/entities/property-feature.entity.ts
@@ -0,0 +1,5 @@
+export class PropertyFeature {
+  id: number;
+  description: string;
+  value: string;
+}
--- src/modules/properties/entities/property.entity.ts
+++ src/modules/properties/entities/property.entity.ts
@@ -1,12 +1,11 @@
+import { PropertyFeature } from './property-feature.entity';
+import { PropertyType } from './property-type.entity';
+
 export class Property {
   id: number;
   address: string;
-  price: number;
-  bedrooms: number;
-  bathrooms: number;
-  squareFeet: number;
-  type: string; // Ex: 'Apartment', 'House', 'Condo', etc.
   description: string;
-  amenities: string[]; // Lista de comodidades
-  imageUrl: string; // URL da imagem representativa
+  images: string[];
+  type: PropertyType;
+  features: PropertyFeature[];
 }
