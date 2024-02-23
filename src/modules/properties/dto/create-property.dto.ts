1001ad91f18a8aa8320001249f22590b129d4807
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2024-02-23 04:21:34 -0300

    feat: property - create payload validatons

--- src/modules/properties/dto/create-property.dto.ts
+++ src/modules/properties/dto/create-property.dto.ts
@@ -1,1 +1,35 @@
-export class CreatePropertyDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePropertyDto {
+  @ApiProperty({
+    description: 'Property name or title',
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(150)
+  name: string;
+
+  @ApiProperty({
+    description: 'Property description or details.',
+  })
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(1500)
+  description?: string;
+
+  @ApiProperty({
+    description:
+      'Property type id. A listing of available property types is provided in "/properties/types"',
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  propertyTypeId: number;
+}
--- src/modules/properties/entities/property.entity.ts
+++ src/modules/properties/entities/property.entity.ts
@@ -1,7 +1,7 @@
 export abstract class Property {
   id: string;
   name: string;
-  description: string;
+  description?: string;
   propertyTypeId: number;
   createdAt: Date;
   updatedAt: Date;
