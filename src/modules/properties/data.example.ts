e64ff5c037b2132238a1f0937d5c93472ac2414c
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2024-02-23 03:49:45 -0300

    docs: property resources - example of response

---
+++ src/modules/properties/data.example.ts
@@ -0,0 +1,21 @@
+export const propertyFeaturesListExample = [
+  {
+    id: 11,
+    name: 'quarto',
+  },
+  {
+    id: 15,
+    name: 'banheiro',
+  },
+];
+
+export const propertyTypesListExample = [
+  {
+    id: 6,
+    name: 'casa',
+  },
+  {
+    id: 7,
+    name: 'apartamento',
+  },
+];
--- src/modules/properties/properties.controller.ts
+++ src/modules/properties/properties.controller.ts
@@ -10,6 +10,11 @@ import {
 import { PropertiesService } from './properties.service';
 import { CreatePropertyDto } from './dto/create-property.dto';
 import { UpdatePropertyDto } from './dto/update-property.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  propertyFeaturesListExample,
+  propertyTypesListExample,
+} from './data.example';
 
 @Controller('properties')
 export class PropertiesController {
@@ -43,11 +48,33 @@ export class PropertiesController {
     return this.propertiesService.remove(+id);
   }
 
+  @ApiOperation({
+    summary:
+      'List of available types of properties available for property registration.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK.',
+    schema: {
+      example: propertyTypesListExample,
+    },
+  })
   @Get('/resources/types')
   listAvailablePropertyTypes() {
     return this.propertiesService.listAvailablePropertyTypes();
   }
 
+  @ApiOperation({
+    summary:
+      'List of available features of properties available for property registration.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK.',
+    schema: {
+      example: propertyFeaturesListExample,
+    },
+  })
   @Get('/resources/features')
   listAvailablePropertyFeatures() {
     return this.propertiesService.listAvailablePropertyFeatures();
